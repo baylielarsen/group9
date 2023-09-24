7bcfe6ef453f6e8b9f31ed036368c8982970775b
philkaplan <kaplanpb@bc.edu>
2023-09-24 18:45:07 -0400

    Commit 1

---
+++ Module_3_P_Final.R
@@ -0,0 +1,81 @@
+#Combine the code above so that you can establish the anole.log data tibble.
+
+anole <- read_csv("anole.dat.csv")
+anole.eco <- read_csv("anole.eco.csv")
+
+anole2 <- anole %>% 
+  left_join(anole.eco) %>% 
+  filter(!Ecomorph %in%c("U", "CH")) %>% 
+  na.omit() %>% 
+  print()
+
+anole.log <- anole2%>%
+  mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
+
+
+#Using the log-transformed data, construct two simple linear models that assess the effect of perch diameter and height by including these as covariates in your models. Be sure to use + notation rather than *, assuming there is no interaction (there isn’t, trust me!).
+
+  #Linear Model Assessing Perch Diameter (ArbPD)
+Anole.log.ArbPD.lm <- lm(HTotal~SVL + ArbPD, anole.log)
+
+  #Linear Model Assessing Perch Height (PH)
+Anole.log.PH.lm <- lm(HTotal~SVL + PH, anole.log)
+
+#Explore how both perch diameter and height effect the hindlimb-SVL relationship by plotting the residuals of your simple linear models against these discrete factors. This will require mutating a data tibble to include residuals from both models. Please produce two separate plots.
+
+  #Mutating Tibble 
+anole.log <- anole.log %>% 
+  mutate(res1 = residuals(Anole.log.ArbPD.lm)) %>% 
+  mutate(res2 = residuals(Anole.log.PH.lm))
+
+  #ArbPD Residuals 
+ArbPD.Res.Graph <- anole.log%>%
+  ggplot(aes(x=ArbPD,y=res1)) +geom_point()
+print(ArbPD.Res.Graph)
+  
+  #PH Residuals 
+PH.Res.Graph <- anole.log%>%
+  ggplot(aes(x=PH,y=res2)) +geom_point()
+print(PH.Res.Graph)
+
+#Under a BM model of trait evolution and using the tree provided, construct phylogenetic least squares models of the hindlimb-SVL relationships that include the unique combinations of these two covariates, i.e,
+
+  #Loading anole.tree
+anole.tree <- read.tree("anole.tre")
+
+  #A PGLS model with the hinglimb~SVL relationship + no other covariates
+
+Anole.BM1 <- gls(HTotal ~SVL, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+  #A PGLS model with the hindlimb-SVL relationship + perch diameter
+Anole.BM2 <- gls(HTotal ~SVL + ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+  
+  #A PGLS model with the hindlimb-SVL relationship + perch height
+
+Anole.BM3 <- gls(HTotal ~SVL + PH , correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+  #A PGSL model with the hindlimb-SVL relationship + perch height + perch diameter
+
+Anole.BM4 <- gls(HTotal ~SVL + PH + ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+#Assess the fit of each of these three models using AICc and AICw and comment on (with comments in the script) whether one or both of the covariates is a significant predictor of hinglimb length in a phylogenetic context.
+
+anole.phylo.aic <- AICc(Anole.BM1,Anole.BM2,Anole.BM3,Anole.BM4)
+aicw(anole.phylo.aic$AICc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Produce a plot of your own design that concisely visualizes the effect of your covariate(s) and factors (i.e., ecomorph) on the hindlimb residuals of the best fitting PGLS model.
+
+
+#Commit your script to your group repository.
